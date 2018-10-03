104fdc0d6ad8bce802dda2f242522113eddda6ad
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2018-10-03 14:17:37 +0300

    Add basic Game component

---
+++ src/Game.js
@@ -0,0 +1,17 @@
+import { Application } from 'pixi.js'
+
+class Game extends Application {
+
+    constructor({
+        width,
+        height
+    }) {
+        super({
+            width,
+            height
+        })
+    }
+
+}
+
+export { Game }
